ff073ca92efce69271622804fec01222d5e7e7a2
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-10-12 10:05:38 +0200

    commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8" />
-  <title>Memory Game CSS Only</title>
+  <title>Memory Game </title>
   <link rel="stylesheet" href="./assets/css/style.css" />
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
   
